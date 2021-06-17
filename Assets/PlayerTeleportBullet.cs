52e75a1946dc2fce69c04e8bc2043f5a2aa92dd0
Mehrad Jafari <mehradjafarii@gmail.com>
2021-06-17 10:47:22 -0400

    level edits , added teleport

---
+++ Assets/PlayerTeleportBullet.cs
@@ -0,0 +1,63 @@
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+public class PlayerTeleportBullet : MonoBehaviour
+{
+    [SerializeField] public KeyCode inputCode = KeyCode.G;
+    [SerializeField] float cooldown = 1;
+    [SerializeField] GameObject teleportBulletPrefab;
+    Character player;
+    float currentCooldown;
+    private TeleportBulletController teleportBulletController;
+
+    private void Awake()
+    {
+        if (teleportBulletPrefab == null)
+        {
+            Debug.LogError("Missing teleport bullet prefab.");
+        }
+        player = GetComponent<Character>();
+    }
+
+    private void Update()
+    {
+        if (currentCooldown > 0)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(inputCode))
+        {
+            if (teleportBulletController == null && currentCooldown <= 0)
+            {
+                currentCooldown = cooldown;
+                var currentBullet = Instantiate(teleportBulletPrefab, transform.position, Quaternion.identity);
+                var projectile = currentBullet.GetComponent<Projectile>();
+                teleportBulletController = currentBullet.GetComponent<TeleportBulletController>();
+                teleportBulletController.Player = this;
+                teleportBulletController.LifeTimer = cooldown;
+                var direction = Vector3.zero;
+
+                direction.y = Input.GetAxisRaw("Player1_Vertical");
+
+                if (direction.y == 0)
+                {
+                    direction.x = player.IsFacingRight ? 1 : -1;
+                }
+
+                projectile.SetDirection(direction, Quaternion.identity);
+            }
+            else if (teleportBulletController != null)
+            {
+                teleportBulletController.TeleportPlayer();
+                teleportBulletController = null;
+            }
+        }
+    }
+
+    internal void Teleport(Vector3 pos)
+    {
+        transform.position = pos;
+        teleportBulletController = null;
+    }
+}
---
+++ Assets/TeleportBulletController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TeleportBulletController : MonoBehaviour
+{
+    [SerializeField] float proximityToWall = 1;
+
+    public float LifeTimer { get; set; }
+    public PlayerTeleportBullet Player { get; set; }
+
+    private void Awake()
+    {
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    private void Update()
+    {
+        LifeTimer -= Time.deltaTime;
+        if (LifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TeleportPlayer()
+    {
+        var pos = transform.position;
+        pos.z = 0;
+        pos = Vector3.MoveTowards(pos, Player.transform.position, proximityToWall);
+        Player.Teleport(pos);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Platforms"))
+        {
+            TeleportPlayer();
+        }
+    }
+}
