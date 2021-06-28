740a5f50fa334500d4c11dd2238bc8adf5b9796c
Mehrad Jafari <mehradjafarii@gmail.com>
2021-06-28 09:12:24 -0400

    camera zoom script added

---
+++ Assets/CameraZoomTrigger.cs
@@ -0,0 +1,28 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraZoomTrigger : MonoBehaviour
+{
+    [SerializeField] float lensSize = 10f;
+
+    private void Awake()
+    {
+        var collider = GetComponent<Collider2D>();
+        if (collider == null)
+        {
+            Debug.LogError("Missing a 2D collider.");
+        }
+        else
+        {
+            collider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Camera.main.orthographicSize = lensSize;
+        }
+    }
+}
